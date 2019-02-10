f6945dd027a96f9e8476b5472e70107a8edf47e9
Madhanroytz <47450640+Madhanroytz@users.noreply.github.com>
2019-02-10 22:59:41 +0530

    Create Hello.java

---
+++ Hello.java
@@ -0,0 +1,15 @@
+import java.util.Scanner;
+
+public class Hello {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Scanner sc=new Scanner(System.in);
+		int a=sc.nextInt();
+		for(int i=0;i<a;i++)
+		{
+			System.out.println("Hello");
+		}
+	}
+
+}
