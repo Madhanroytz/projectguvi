0709fb65d9edd231f7f6e36ddbddb282e0c34842
Madhanroytz <47450640+Madhanroytz@users.noreply.github.com>
2019-02-10 22:29:44 +0530

    Create  Greater.java

---
+++ Greater.java
@@ -0,0 +1,27 @@
+import java.util.Scanner;
+
+public class Greater {
+
+	public static void main(String[] args) {
+		Scanner sc=new Scanner(System.in);
+		int a=sc.nextInt();
+	int[] b=new int[5];
+	int max=b[0];
+		for(int i=0;i<a;i++)
+		{
+			b[i]=sc.nextInt();
+			 while(b[i]>max)
+		      {
+		    	  max=b[i];
+		    	 
+		      }
+		}
+		 System.out.println(max);
+      //int max=b[0];
+    //  for(int j=0;j<a;j++)
+      //{
+     
+      }
+	}
+
+
