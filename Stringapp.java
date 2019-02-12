cac7825cf1296bb881c05cc68984fd23e92f09dc
Madhanroytz <47450640+Madhanroytz@users.noreply.github.com>
2019-02-12 22:43:57 +0530

    Create Stringapp.java

---
+++ Stringapp.java
@@ -0,0 +1,14 @@
+package set1;
+
+import java.util.Scanner;
+
+public class Stringapp {
+
+	public static void main(String[] args) {
+    Scanner sc=new Scanner(System.in);
+    StringBuffer sb=new StringBuffer(sc.next());
+  System.out.println(sb.append("."));
+    		
+	}
+
+}
