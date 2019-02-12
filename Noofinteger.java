7f9183b0f10004ce3cf76fa57e596ba1e69e26da
Madhanroytz <47450640+Madhanroytz@users.noreply.github.com>
2019-02-12 20:37:06 +0530

    Create Noofinteger.java

---
+++ Noofinteger.java
@@ -0,0 +1,13 @@
+import java.util.Scanner;
+
+public class Noofinteger {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+Scanner s=new Scanner(System.in);
+String k=s.next();
+int len=String.valueOf(k).length();
+System.out.println(len);
+	}
+
+}
