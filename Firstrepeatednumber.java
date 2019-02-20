295ab85740011d5c4ff8e10afa430db28cc74503
Madhanroytz <47450640+Madhanroytz@users.noreply.github.com>
2019-02-20 21:32:07 +0530

    Create Firstrepeatednumber.java

---
+++ Firstrepeatednumber.java
@@ -0,0 +1,35 @@
+import java.util.Scanner;
+
+public class Firstrepeatednumber {
+	static int RepeatedNum()
+	{
+		int N,i,j;
+		Scanner sc=new Scanner(System.in);
+		System.out.println("enter the number:");
+		N=sc.nextInt();
+		int[] array=new int[N];
+		System.out.println("enter the array elements:");
+		for(i=0;i<N;i++)
+		{
+			array[i]=sc.nextInt();
+		}
+		for(i=0;i<N;i++)
+		{
+			for(j=i+1;j<N;j++)
+			{
+				if(array[i]==array[j])
+				{
+					return array[i];
+				}
+			}
+		}
+		return array[i];
+		
+	}
+
+	public static void main(String[] args) {
+		System.out.println("repeated number:"+RepeatedNum());
+
+	}
+
+}
